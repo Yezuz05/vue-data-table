b411bd6584f091a23798cc7500acfeccbc67e51e
Bisiriyu Yusuff <temitopeyussuph@gmail.com>
2018-12-12 16:23:16 +0100

    feat: created datatable component, impored muli font, First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DataTable msg="hello from parent" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DataTable from './components/DataTable.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    DataTable
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  div#app {
+    padding: 30px;
+  }
 </style>
---
+++ src/components/DataTable.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>{{msg}}</h1>
+</template>
+
+<script>
+export default {
+    name: 'DataTable',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
