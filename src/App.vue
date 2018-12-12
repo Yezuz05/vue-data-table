98fc24fab5995e6de3b6194e678f0829ca6c8fdf
Bisiriyu Yusuff <temitopeyussuph@gmail.com>
2018-12-12 18:45:36 +0100

    feta: passing data and columns to table component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,45 @@
 <template>
   <div id="app">
-    <DataTable msg="hello from parent" />
+    <h3>My Custom Data Table</h3>
+    <DataTable :dataSource="payments" :columns="columns" />
   </div>
 </template>
 
 <script>
 import DataTable from './components/DataTable.vue'
+import { payments } from './payments.js'
 
 export default {
   name: 'app',
   components: {
     DataTable
+  },
+  data () {
+    return {
+      payments,
+      columns: [
+        {
+          title: 'ID',
+          dataIndex: 'id'
+        },
+        {
+          title: 'Name',
+          dataIndex: 'name'
+        },
+        {
+          title: 'Description',
+          dataIndex: 'description'
+        },
+        {
+          title: 'Date',
+          dataIndex: 'date'
+        },
+        {
+          title: 'Amount',
+          dataIndex: 'amount'
+        }
+      ]
+    }
   }
 }
 </script>
@@ -18,5 +47,6 @@ export default {
 <style lang="scss">
   div#app {
     padding: 30px;
+    text-align: center;
   }
 </style>
--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,16 +1,44 @@
 <template>
-    <h1>{{msg}}</h1>
+    <div id="data-table">
+        <table class="table">
+            <thead>
+                <tr>
+                    <td v-for="(column, index) in tableColumns" :key="index">{{column.title}}</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(row, rowIndex) in tableData" :key="rowIndex">
+                    <td v-for="(column, index) in tableColumns" :key="index">{{row[column.dataIndex]}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'DataTable',
     props: {
-        msg: String
+        dataSource:Array,
+        columns:Array
+    },
+    data() {
+        return {
+            tableData: this.dataSource,
+            tableColumns: this.columns
+        }
+    },
+    mounted() {
     }
 }
 </script>
 
 <style lang="scss" scoped>
-
+    #data-table {
+        width: 100%;
+        .table {
+            width: inherit;
+            text-align: left;
+        }
+    }
 </style>
